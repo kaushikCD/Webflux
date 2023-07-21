2f1b7752db7409c56ea6fcb02fdfabe46c3e441e
Kaushik B <kaushikb@clouddestinations.com>
2023-07-21 15:09:01 +0530

    Initial commit

--- apiclient/src/main/java/com/webFlux/webflux/demo/api/ApiRequest.java
+++ apiclient/src/main/java/com/webFlux/webflux/demo/api/ApiRequest.java
@@ -1,19 +1,16 @@
-
 package com.webFlux.webflux.demo.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
-
 public class ApiRequest {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Starting a WebFlux API call");
         reactiveEndpoint();
         traditionalEndpoint();
+        Thread.sleep(4000);
     }
     private static void traditionalEndpoint() {
         RestTemplate template = new RestTemplate();
@@ -23,7 +20,7 @@ public class ApiRequest {
     }
     private static CompletableFuture<Void> reactiveEndpoint() {
         WebClient clientCall = WebClient.create("http://localhost:8080");
-         return clientCall.get()
+        return clientCall.get()
                 .uri("/monoReactive")
                 .retrieve()
                 .bodyToMono(String.class)
