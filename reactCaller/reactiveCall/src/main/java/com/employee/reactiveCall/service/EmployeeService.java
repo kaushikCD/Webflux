8ccd3f1ab8e6124d5be89f1585281d81ca3ba5cc
kaushikCD <129848826+kaushikCD@users.noreply.github.com>
2023-07-24 16:16:45 +0530

    initial commit

---
+++ reactCaller/reactiveCall/src/main/java/com/employee/reactiveCall/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.employee.reactiveCall.service;
+
+import com.employee.reactiveCall.entity.Employee;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class EmployeeService {
+    private final Flux<Employee> employees;
+    public EmployeeService() {
+        employees = Flux.just(
+                new Employee(100, "Arjun", 22),
+                new Employee(101, "Kumar", 21)
+        );
+    }
+    public Flux<Employee> getAllEmployees() {
+        return employees;
+    }
+    }
---
+++ reactCaller/reactiveCall/src/test/java/com/employee/reactiveCall/ReactiveCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.reactiveCall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
